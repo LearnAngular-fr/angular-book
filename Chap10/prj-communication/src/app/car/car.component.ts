f1e9aaaef4d09cb7e191e1752db47d891ec1fcf2
WilliamKoza <william.koza@gmail.com>
2017-08-30 20:27:36 +0200

    first push

---
+++ Chap10/prj-communication/src/app/car/car.component.ts
@@ -0,0 +1,44 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'car',
+  template: `
+    <h1><span> Clignolant gauche : {{blinkerLeft}} </span> // <span> Clignolant droit : {{blinkerRight}} </span></h1>
+    <h1><span> Porte gauche : {{doorLeft}}  </span> // <span> Porte droite : {{doorRight}}</span></h1>
+     
+     <ng-content select="constructor"></ng-content>
+  `
+})
+export class CarComponent {
+
+  public blinkerLeft: boolean;
+  public blinkerRight: boolean;
+  public doorLeft: boolean;
+  public doorRight: boolean;
+
+
+  constructor() {
+    this.blinkerLeft = false;
+    this.blinkerRight = false;
+    this.doorLeft = false;
+    this.doorRight = false;
+  }
+
+
+  blinkerLeftSwitch(): void {
+    this.blinkerLeft = !this.blinkerLeft;
+  }
+
+  blinkerRightSwitch(): void {
+    this.blinkerRight = !this.blinkerRight;
+  }
+
+  doorLeftSwitch(): void {
+    this.doorLeft = !this.doorLeft;
+  }
+
+  doorRightSwitch(): void {
+    this.doorRight = !this.doorRight;
+  }
+
+}
---
+++ Chap9/mon-projet-routage/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {AccueilComponent} from "./accueil/accueil.component";
+import {Page1Component} from "./page1/page1.component";
+
+const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'page1', component: Page1Component },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
