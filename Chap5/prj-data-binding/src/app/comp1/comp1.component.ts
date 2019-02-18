357ace4ff5d065753435369307dc33e903868f65
WilliamKoza <william.koza@gmail.com>
2019-02-18 15:37:58 +0100

    erase angular v5

--- Chap5/prj-data-binding/src/app/comp1/comp1.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component, Input} from '@angular/core';
-@Component({
-  selector: 'app-comp1',
-  template: ` {{monAdresse.street}} `
-})
-export class Comp1Component {
-  @Input() monAdresse: any;
-}
